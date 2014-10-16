54c2703a842814a1c4ad9f587dbac8039b1a9eb9
Balakrishna <BP8G6@mail.umkc.edu>
2014-10-15 22:07:20 -0500

    Increment#1
    eShoppe - An Online Shopping Portal

---
+++ Increment#1/eShoppe/src/com/eshoppe/Registration.java
@@ -0,0 +1,14 @@
+package com.eshoppe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Registration extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.user_registration);
+	}
+	
+}
