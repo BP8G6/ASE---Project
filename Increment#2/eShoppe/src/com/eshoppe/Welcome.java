28e1ff6d92fddbe88393298620da8bf18791957e
Balakrishna <BP8G6@mail.umkc.edu>
2014-11-03 10:05:40 -0600

    Increment#2
    Increment#2

---
+++ Increment#2/eShoppe/src/com/eshoppe/Welcome.java
@@ -0,0 +1,14 @@
+package com.eshoppe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Welcome extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.welcome);
+		
+	}
+}
